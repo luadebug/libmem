d6c8e78b984ffd6abf8342506c519125b961408f
Saikari <lin@sz.cn.eu.org>
2025-12-13 20:47:42 +0300

    Implement MinGW builds support

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,4 +1,4 @@
-if(WIN32) # this MUST NOT match CYGWIN
+if(WIN32 AND NOT MINGW) # this MUST NOT match CYGWIN or MinGW cross-compilation
 	set(CMAKE_GENERATOR "NMake Makefiles" CACHE INTERNAL "" FORCE)
 else()
 	set(CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
